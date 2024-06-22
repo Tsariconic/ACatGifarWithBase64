954a39a4c5457d302d8fa136dbbd08f6414d0aed
Tsariconic <122738520+Tsariconic@users.noreply.github.com>
2024-06-22 20:40:09 +0100

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        System.out.println("VGhpcyBqYXZhc2NyaXB0IGNvZGUgaXMgaGlkZGVuIGluIGEgR0lGLCBidXQgSSBlbmNvZGVkIGl0IGNvcyBpIGhhdGUgdS4gOik=");
     }
 }
